f5084cfc64aac76cf84c6eed7e79e72f2d91d82d
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-13 17:20:29 -0400

    Added edit post controller/view and update post controller method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,23 @@ class PostsController < ApplicationController
     post.destroy
     redirect_to board_topic_path(topic.board, topic), alert: "Post deleted"
   end
+
+  def edit
+    @post = Post.find(params[:id])
+    @user = current_user
+    unless @post.user == @user || @post.board.owner == @user
+      redirect_to board_topic_path(@post.board, @post.topic), alert: "You don't have permission to edit this post"
+    end
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to board_topic_path(post.board, post.topic), notice: "Post successfully updated"
+    else
+      render :edit, notice: "Error updating post"
+    end
+  end
   private
     def post_params
       params.require(:post).permit(:user_id, :topic_id, :title, :body)
