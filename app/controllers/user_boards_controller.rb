3c5f01956225c42e401618017ad9af0de45104f1
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-13 14:49:30 -0400

    Added UserBoardsController methods

--- app/controllers/user_boards_controller.rb
+++ app/controllers/user_boards_controller.rb
@@ -1,2 +1,19 @@
 class UserBoardsController < ApplicationController
+  def index
+    @user_boards = UserBoard.where(user_id: current_user.id)
+  end
+
+  def create
+    if user_board = UserBoard.create(user_board_params)
+      redirect_to user_boards_path
+    else
+      redirect_to user_boards_path, alert: "Error saving board"
+    end
+  end
+
+  def destroy
+    user_board = UserBoard.find(params[:id])
+    user_board.destroy
+    redirect_to user_boards_path
+  end
 end
