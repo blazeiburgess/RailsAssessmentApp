5b8f869a028ffd2366e482c81334785cd41fd3f4
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-13 20:56:03 -0400

    Added board class methods to welcome page view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def home
-
+    @random_topics = Board.random_topics
+    @recent_topics = Board.recent_topics
   end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -3,6 +3,14 @@ class Board < ApplicationRecord
   has_many :posts, through: :topics
   belongs_to :owner, class_name: "User"
 
+  def self.random_topics
+    all.map(&:topics).flatten.shuffle.first(10)
+  end
+
+  def self.recent_topics
+    all.map(&:topics).flatten.sort {|a, b| a <=> b}.reverse.first(10)
+  end
+
   def last_10_topics
     posts.order("id DESC").map(&:topic).uniq.first(10)
   end
