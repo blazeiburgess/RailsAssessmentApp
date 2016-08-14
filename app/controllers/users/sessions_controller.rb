74dacfbf6f5f78ee66d796c5b6fd3f3f095786ec
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-14 10:27:51 -0400

    Added manual controls for github user auth/creation

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
+  require 'securerandom'
+
 
   # GET /resource/sign_in
   # def new
@@ -7,16 +9,31 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    if auth_hash
+      if user = User.find_by(email: auth_hash[:info][:nickname] + "@github.auth")
+	sign_in(user)
+	
+      else
+	user = User.create(email: auth_hash[:info][:nickname] + "@github.auth", password: SecureRandom.hex)
+	sign_in(user)
+      end
+	
+      redirect_to root_path, notice: "Signed in successfully!"
+    else
+      super
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
+  protected
+    def auth_hash
+      request.env['omniauth.auth']
+    end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
